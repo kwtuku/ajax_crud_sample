f44a0156ecc442a6876e867c85b7ff8d6e72fb0e
kwtuku <ttyttyttitti@gmail.com>
2021-03-03 22:21:00 +0900

    メッセージの削除機能を追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: %i[destroy]
   def index
     @messages = Message.all
   end
@@ -7,8 +8,16 @@ class MessagesController < ApplicationController
     @message = Message.create!(message_params)
   end
 
+  def destroy
+    @message.destroy!
+  end
+
   private
     def message_params
       params.require(:message).permit(:content)
+    end
+
+    def set_message
+      @message = Message.find(params[:id])
     end
 end
